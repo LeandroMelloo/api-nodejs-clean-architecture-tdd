2fc73d081ab91511ec8aacedbc283c4948613bdb
LeandroMelloo <leandropaulino@brq.com>
2021-06-15 16:25:32 -0300

    implementando o backend

---
+++ login.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router
+const mongoose = require('mongoose')
+const AccountModel = mongoose.model('Account')
+
+module.exports = () => {
+  router.get('/signup', )
+}
